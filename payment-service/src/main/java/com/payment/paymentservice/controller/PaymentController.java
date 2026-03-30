373f10620f70c29a98894e2f807aec721103b394
dil816 <rivindudilochana18329@gmail.com>
2026-03-30 21:57:01 +0530

    mistake

--- payment-service/src/main/java/com/payment/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/payment/paymentservice/controller/PaymentController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("//api/payments")
+@RequestMapping("/api/payments")
 @RequiredArgsConstructor
 public class PaymentController {
 
