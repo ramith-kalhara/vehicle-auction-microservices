75449f7b2c9f01450ef84575c240da1497513e4b
ramith-kalhara <67057497+ramith-kalhara@users.noreply.github.com>
2026-04-11 10:42:01 +0530

    commit merge all file

--- auction-service/src/main/java/com/vehicle/auction/service/impl/AuctionServiceImpl.java
+++ auction-service/src/main/java/com/vehicle/auction/service/impl/AuctionServiceImpl.java
@@ -1,4 +1,4 @@
-package main.java.com.vehicle.auction.service.impl;
+package com.vehicle.auction.service.impl;
 
 import com.vehicle.auction.dto.AuctionRequestDto;
 import com.vehicle.auction.dto.AuctionResponseDto;
